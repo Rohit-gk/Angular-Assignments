11aa5220fa0f1e28729f1692afc55e7a3fff9dba
rohitincubxperts <rohitka@incubexperts.com>
2022-06-21 10:44:13 +0530

    nested module assignment is completed

---
+++ src/app/admin/customer/customer-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './add/add.component';
+import { CustomerComponent } from './customer.component';
+import { EditComponent } from './edit/edit.component';
+import { ListComponent } from './list/list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CustomerComponent,
+    children: [
+      { path: '', component: ListComponent },
+      { path: 'add', component: AddComponent },
+      { path: 'list', component: ListComponent },
+    ]
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CustomerRoutingModule { }
---
+++ src/app/admin/customer/customer.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerRoutingModule } from './customer-routing.module';
+import { CustomerComponent } from './customer.component';
+import { ListComponent } from './list/list.component';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
+
+@NgModule({
+  declarations: [CustomerComponent, ListComponent, AddComponent, EditComponent],
+  imports: [CommonModule, CustomerRoutingModule],
+})
+export class CustomerModule { }
